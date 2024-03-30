d5bea705dbce346e68a20936de4c737e0f2b2ec0
Christophe Thibaut <cthibauttof@gmail.com>
2024-03-30 13:49:30 +0100

    test with a table

--- features/factorial.feature
+++ features/factorial.feature
@@ -3,3 +3,12 @@ Feature: Factorial
     Scenario: Compute factorial
         When I ask for factorial 6
         Then I should get the result 720
+
+    Scenario: Compute factorials
+        When I ask for factorials
+        Then I should get the results
+            | argument | result |
+            | 1        | 1      |
+            | 2        | 2      |
+            | 3        | 6      |
+            | 4        | 24     |
--- features/support/factorial.js
+++ features/support/factorial.js
@@ -9,3 +9,16 @@ When('I ask for factorial {int}', function(x) {
 Then('I should get the result {int}', function(expectedResult) {
     assert.equal(factorial(this.argument), expectedResult)
 });
+
+ When('I ask for factorials', function () { });
+
+Then('I should get the results', function (table) {
+    for (let i = 0; i < table.hashes().length; i++) {
+        let h = table.hashes()[i]
+        console.log(h)
+        let x = h['argument']
+        let r = h['result']
+        assert.equal(r, factorial(x))
+    }
+});
+
